64ac1d0db21c2f222d8f7a7862c5226248d35ba7
nzsombor04 <nzsombor04@gmail.hu>
2024-12-13 15:30:24 +0100

    RestaurantLogic added

--- Logic/Logic/RestaurantLogic.cs
+++ Logic/Logic/RestaurantLogic.cs
@@ -1,5 +1,7 @@
 ﻿using Data;
+using Entities.Dtos.Restaurant;
 using Entities.Models;
+using Logic.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,48 @@ namespace Logic.Logic
     public class RestaurantLogic
     {
         Repository<Restaurant> repo;
-        
+        DtoProvider dtoProvider;
+
+        public RestaurantLogic(Repository<Restaurant> repo, DtoProvider dtoProvider)
+        {
+            this.repo = repo;
+            this.dtoProvider = dtoProvider;
+        }
+
+        public void AddRestaurant(RestaurantCreateUpdateDto dto)
+        {
+            Restaurant r = dtoProvider.Mapper.Map<Restaurant>(dto);
+
+            if (repo.GetAll().FirstOrDefault(x => x.Address == r.Address) == null)
+            {
+                repo.Create(r);
+            }
+            else
+            {
+                throw new ArgumentException("Az alábbi helyszínen már létezik étterem!");
+            }
+        }
+
+        public IEnumerable<RestaurantShortViewDto> GetAllRestaurants()
+        {
+            return repo.GetAll().Select(x => dtoProvider.Mapper.Map<RestaurantShortViewDto>(x));
+        }
+
+        public void DeleteMovie(string id)
+        {
+            repo.DeleteById(id);
+        }
+
+        public void UpdateMovie(string id, RestaurantCreateUpdateDto dto)
+        {
+            var old = repo.FindById(id);
+            dtoProvider.Mapper.Map(dto, old);
+            repo.Update(old);
+        }
+
+        public RestaurantViewDto GetRestaurant(string id)
+        {
+            return dtoProvider.Mapper.Map<RestaurantViewDto>(repo.FindById(id));
+        }
     }
 }
