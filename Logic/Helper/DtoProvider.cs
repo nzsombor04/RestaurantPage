62875c932c22e355b6ff0876654b3d538c2cf940
nzsombor04 <nzsombor04@gmail.hu>
2024-12-13 15:24:08 +0100

    DtoProvider added

---
+++ Logic/Helper/DtoProvider.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Entities.Dtos.Restaurant;
+using Entities.Dtos.Review;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Helper
+{
+    public class DtoProvider
+    {
+        public Mapper Mapper { get; }
+
+        public DtoProvider()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Restaurant, RestaurantViewDto>();
+                cfg.CreateMap<Restaurant, RestaurantShortViewDto>();
+                cfg.CreateMap<RestaurantCreateUpdateDto, Restaurant>();
+                cfg.CreateMap<Review, ReviewViewDto>();
+                cfg.CreateMap<ReviewCreateDto, Review>();
+            });
+
+            Mapper = new Mapper(config);
+        }
+    }
+}
---
+++ Logic/Logic/RestaurantLogic.cs
@@ -0,0 +1,16 @@
+﻿using Data;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Logic
+{
+    public class RestaurantLogic
+    {
+        Repository<Restaurant> repo;
+        
+    }
+}
