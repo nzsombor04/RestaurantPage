7ff62ad86f43552f1783ff58b76b3fbb8fe17ab8
nzsombor04 <nzsombor04@gmail.hu>
2024-12-13 13:12:47 +0100

    Item added

---
+++ Entities/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Item
+    {
+        public Item(string name, string description, int price)
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+        }
+
+        [Key]
+        [StringLength(50)]
+        public string Id { get; set; } 
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public int Price { get; set; }
+    }
+}
