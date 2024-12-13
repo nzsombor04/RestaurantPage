342014588796e2ead1c509a42d166be788cc73bd
nzsombor04 <nzsombor04@gmail.hu>
2024-12-13 14:58:59 +0100

    RestaurantDto added

---
+++ Endpoint/Controllers/RestaurantController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+
+    }
+}
