b1a46f0d8bc2ab4a9cac9fd22f0d8c63e2758916
nzsombor04 <nzsombor04@gmail.hu>
2024-12-14 13:57:49 +0100

    UserCreateDto added

---
+++ Entities/Dtos/User/UserCreateDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.User
+{
+    public class UserCreateDto
+    {
+        [MinLength(1)]
+        public required string Email { get; set; } = "";
+
+        [MinLength(1)]
+        public required string FirstName { get; set; } = "";
+
+        [MinLength(1)]
+        public required string LastName { get; set; } = "";
+
+        [MinLength(5)]
+        public required string Username { get; set; } = "";
+
+        [MinLength(5)]
+        public required string Password { get; set; } = "";
+    }
+}
