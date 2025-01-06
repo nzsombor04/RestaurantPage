5fc4e7d34a50eff98a0b82416df99a3b5c3ae9a7
nzsombor04 <nzsombor04@gmail.hu>
2025-01-06 11:38:33 +0100

    phone number removed from RestaurantShortView

--- Entities/Dtos/Restaurant/RestaurantShortViewDto.cs
+++ Entities/Dtos/Restaurant/RestaurantShortViewDto.cs
@@ -11,7 +11,6 @@ namespace Entities.Dtos.Restaurant
         public string Id { get; set; } = "";
         public string Name { get; set; } = "";
         public string Address { get; set; } = "";
-        public string Phone { get; set; } = "";
         public double AvarageRating { get; set; } = 0;
     }
 }
